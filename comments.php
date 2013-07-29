ccfd9950f79e3f56a2ebf1d3c131a384b644099c
SingleX <ivy16264355@gmail.com>
2013-07-29 13:55:49 +0800

    some changes

--- comments.php
+++ comments.php
@@ -13,9 +13,7 @@
  */
 
 /*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
+ * 有密码保护的文章，未输密码时返回。
  */
 if ( post_password_required() )
 	return;
--- single.php
+++ single.php
@@ -15,11 +15,11 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', get_post_format() ); ?>
-
+<!-- 修改了前后文章显示样式 -->
 				<nav class="nav-single">
 					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
-					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+					<span class="nav-previous"><?php previous_post_link( '%link', '<i class="icon-chevron-left"></i>%title' ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', '%title<i class="icon-chevron-right"></i>' ); ?></span>
 				</nav><!-- .nav-single -->
 
 				<?php comments_template( '', true ); ?>
