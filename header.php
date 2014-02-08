1644c980b2475e6d56fc77d9e0bf2f4a5de4b59e
SingleX <ivy16264355@gmail.com>
2014-02-08 15:34:44 +0800

    add headcode

--- header.php
+++ header.php
@@ -42,6 +42,9 @@ if(is_home()){
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
+<?php if($options['headcode']) : ?>
+<?php echo($options['headcode']); ?>
+<?php endif; ?>
 <?php wp_head(); ?>
 </head>
 
