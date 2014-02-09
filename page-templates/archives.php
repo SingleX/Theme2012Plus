2a0aab91a13e9bd1024309c4478121bb42781b2a
SingleX <ivy16264355@gmail.com>
2014-02-09 21:30:12 +0800

    repare archives display

--- page-templates/archives.php
+++ page-templates/archives.php
@@ -5,7 +5,6 @@ Template Name: archives(存档页)
 ?>
 
 <?php get_header(); ?>
-
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -13,9 +12,21 @@ Template Name: archives(存档页)
 				<a id="expand_collapse" href="#">全部展开/收缩</a>
 				<div id="archives"><?php theme2012plus_archives_list(); ?></div>
 			<?php endwhile; // end of the loop. ?>
-
 		</div><!-- #content -->
 	</div><!-- #primary -->
-	<script src="<?php echo get_template_directory_uri(); ?>/js/theme2012plus.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+$('#expand_collapse,.archives-yearmonth').css({cursor:"s-resize"});
+$('#archives ul li ul.archives-monthlisting').hide();
+$('#archives ul li ul.archives-monthlisting:first').show();
+$('#archives ul li span.archives-yearmonth').click(function(){$(this).next().slideToggle('fast');return false;});
+$('#expand_collapse').toggle(
+function(){
+$('#archives ul li ul.archives-monthlisting').slideDown('fast');
+},
+function(){
+$('#archives ul li ul.archives-monthlisting').slideUp('fast');
+});
+</script>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
