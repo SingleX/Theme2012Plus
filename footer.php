9416f5fb841bb44c06f9745f802e8a6c55f01250
SingleX <singlexer@gmail.com>
2014-02-11 14:47:24 +0800

    Update footer.php
    add theme info

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+	//如果你喜欢这份主题，请保留页脚主题信息，谢谢 --By SingleX
 	$options = get_option('singlex_options');
 ?>
 	</div><!-- #main .wrapper -->
@@ -6,7 +7,7 @@
 		<div class="site-info">
 			<p class="left">&copy; 2011-2013 · <a href="<?php bloginfo('url');?>"><?php bloginfo('name');?></a></p>
 			<p class="middle">总浏览量 : <?php echo lo_all_view(); ?></p>
-			<p class="right">WordPress &amp; Theme2012Plus</p>
+			<p class="right"><a href="http://www.singlex.net" target="_blank">Theme2012Plus</a></p>
 			<p class="hide"><?php echo($options['footercode']); ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
